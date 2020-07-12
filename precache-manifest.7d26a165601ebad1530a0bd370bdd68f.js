bf0db54efea3b98fb4e0a509a2ed79a72d1af6ab
vedha-24 <56401001+vedha-24@users.noreply.github.com>
2020-07-11 23:57:07 -0700

    Updates

---
+++ precache-manifest.7d26a165601ebad1530a0bd370bdd68f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0506debf70e3da195ac6b2d05f9dbd9d",
+    "url": "/ToDoList-In-React/index.html"
+  },
+  {
+    "revision": "2167f23da38f358485bd",
+    "url": "/ToDoList-In-React/static/css/main.86fa7c33.chunk.css"
+  },
+  {
+    "revision": "bd0c97b7a831b9122b2d",
+    "url": "/ToDoList-In-React/static/js/2.0512a152.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList-In-React/static/js/2.0512a152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2167f23da38f358485bd",
+    "url": "/ToDoList-In-React/static/js/main.634ec7b9.chunk.js"
+  },
+  {
+    "revision": "9ccfbe47f3867e03b594",
+    "url": "/ToDoList-In-React/static/js/runtime-main.552146e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDoList-In-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
